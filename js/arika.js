f4b64e99c4f4efefd995cc203d40c7f33fd3c708
Anshika15 <bansalanshika20@gmail.com>
2020-12-16 00:29:42 +0530

    web Application

---
+++ js/arika.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$('#a').click(function(){
+		$(this).addClass("active") ;
+
+	});
+	$('#b').click(function(){
+		$(this).addClass("active") ;
+
+	});
+	$('#c').click(function(){
+		$(this).addClass("active") ;
+
+	});
+	$('#d').click(function(){
+		$(this).addClass("active") ;
+
+	});
+	$('#e').click(function(){
+		$(this).addClass("active") ;
+
+	});
+	$('#f').click(function(){
+		$(this).addClass("active") ;
+
+	});
+	$('#g').click(function(){
+		$(this).addClass("active") ;
+
+	});
+	$('#h').click(function(){
+		$(this).addClass("active") ;
+
+	});
+	$('#i').click(function(){
+		$(this).addClass("active") ;
+
+	});
+});
